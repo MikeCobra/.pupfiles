947e7a2586cf87e6417092c7b8286bade09a16d6
Mike Clarke <michaelclarkecs@gmail.com>
2015-11-01 21:14:29 +0000

    Add pathogen and vim modules

--- modules/vim/manifests/init.pp
+++ modules/vim/manifests/init.pp
@@ -2,4 +2,56 @@ class vim {
   package { 'vim':
     ensure => latest,
   }
+
+  file { '/home/michaelc/.vimrc':
+    ensure => file,
+    source => 'puppet:///modules/vim/vimrc',
+    owner  => 'michaelc',
+    group  => 'michaelc',
+    mode   => '0644',
+  }
+
+  file { '/home/michaelc/.vim/bundle':
+    ensure => directory,
+    owner  => 'michaelc',
+    group  => 'michaelc',
+    mode   => '0755',
+  }
+
+  file { '/home/michaelc/.vim/autoload':
+    ensure => directory,
+    owner  => 'michaelc',
+    group  => 'michaelc',
+    mode   => '0755',
+  }
+
+  file { '/home/michaelc/.vim/autoload/pathogen.vim':
+    ensure  => file,
+    require => File['/home/michaelc/.vim/autoload'],
+    source  => 'puppet:///modules/vim/vimrc',
+    owner   => 'michaelc',
+    group   => 'michaelc',
+    mode    => '0644',
+  }
+
+  vcsrepo { '/home/michaelc/.vim/bundle/sensible.vim':
+    ensure   => present,
+    require => File['/home/michaelc/.vim/bundle'],
+    provider => git,
+    source   => 'git://github.com/tpope/vim-sensible.git',
+  }
+
+  vcsrepo { '/home/michaelc/.vim/bundle/nerdtree.vim':
+    ensure   => present,
+    require  => File['/home/michaelc/.vim/bundle'],
+    provider => git,
+    source   => 'https://github.com/scrooloose/nerdtree.git',
+  }
+
+  vcsrepo { '/home/michaelc/.vim/bundle/puppet.vim':
+    ensure   => present,
+    require  => File['/home/michaelc/.vim/bundle'],
+    provider => git,
+    source   => 'git://github.com/rodjek/vim-puppet.git',
+  }
 }
