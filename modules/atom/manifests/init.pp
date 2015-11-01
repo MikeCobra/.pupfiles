a4e48b254c9b7183bc87b5fd318bb20213396e59
Mike Clarke <michaelclarkecs@gmail.com>
2015-11-01 22:41:20 +0000

    Add signing key

--- modules/atom/manifests/init.pp
+++ modules/atom/manifests/init.pp
@@ -3,6 +3,11 @@ class atom {
 
   apt::ppa { 'http://ppa.launchpad.net/webupd8team/atom/ubuntu': }
 
+  apt::key { 'webupd8team/atom':
+    id      => '7B2C3B0889BF5709A105D03AC2518248EEA14886',
+    server  => 'ppa.launchpad.net',
+  }
+
   package { 'atom':
     ensure  => latest,
     require => Apt::PPA['http://ppa.launchpad.net/webupd8team/atom/ubuntu'],
