c7d44853e4183c601045e9a3f9a9dccb895c6433
Mike Clarke <michaelclarkecs@gmail.com>
2015-11-01 23:09:33 +0000

    Fix atom permissions

--- modules/atom/manifests/init.pp
+++ modules/atom/manifests/init.pp
@@ -13,11 +13,18 @@ class atom {
       Apt::PPA['ppa:webupd8team/atom'],
       Apt::Key['ppa:webupd8team/atom'],
     ],
+    notify  => Exec['atom-permissions'],
   }
 
   package { 'language-puppet':
-    ensure => latest,
-    require => Package['atom'],
+    ensure   => latest,
+    require  => Package['atom'],
     provider => 'apm',
+    notify   => Exec['atom-permissions'],
+  }
+
+  exec { 'atom-permissions':
+    command => 'chown -R michaelc:michaelc /home/michaelc/.atom',
+    refreshonly => true,
   }
 }
