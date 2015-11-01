f9935eecc502d31a253c0bc84703001d9844c90b
Mike Clarke <michaelclarkecs@gmail.com>
2015-11-01 22:47:25 +0000

    Fix PPA definition

--- modules/atom/manifests/init.pp
+++ modules/atom/manifests/init.pp
@@ -1,15 +1,17 @@
 class atom {
   include apt
 
-  apt::ppa { 'http://ppa.launchpad.net/webupd8team/atom/ubuntu': }
+  apt::ppa { 'ppa:webupd8team/atom': }
 
-  apt::key { 'webupd8team/atom':
-    id      => '7B2C3B0889BF5709A105D03AC2518248EEA14886',
-    server  => 'ppa.launchpad.net',
+  apt::key { 'ppa:webupd8team/atom':
+    id => '7B2C3B0889BF5709A105D03AC2518248EEA14886',
   }
 
   package { 'atom':
     ensure  => latest,
-    require => Apt::PPA['http://ppa.launchpad.net/webupd8team/atom/ubuntu'],
+    require => [
+      Apt::PPA['ppa:webupd8team/atom'],
+      Apt::Key['ppa:webupd8team/atom'],
+    ],
   }
 }
