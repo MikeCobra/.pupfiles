f6268aaefc45cb54e18448a377f72f0c00ec6fa2
Mike Clarke <michaelclarkecs@gmail.com>
2015-11-01 18:10:21 +0000

    Add ohmyzsh

--- modules/zsh/manifests/init.pp
+++ modules/zsh/manifests/init.pp
@@ -2,4 +2,16 @@ class zsh {
   package { 'zsh':
     ensure => latest,
   }
+
+  class { 'ohmyzsh': }
+
+  ohmyzsh::install { 'michaelc': }
+
+  ohmyzsh::theme { [ 'michaelc' ]:
+    theme => 'steeef'
+  }
+
+  ohmyzsh::plugins { 'michaelc':
+    plugins => 'git command-not-found'
+  }
 }
