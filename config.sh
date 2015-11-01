69e2038c9fa98e7ee88a64ead729fa4272bc456a
Mike Clarke <michaelclarkecs@gmail.com>
2015-11-01 17:55:47 +0000

    Update config.sh to pull down from git

--- config.sh
+++ config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PUPFILES_DIR=~/.pupfiles
+
 if [[ $(id -u) -ne 0 ]]; then
 	echo "This script needs to run as root" 1>&2
 	exit 1
@@ -7,4 +9,13 @@ fi
 
 
 apt-get install -y git
-/opt/puppetlabs/bin/puppet apply --modulepath ~/.pupfiles/modules ~/.pupfiles/manifests/desktop.pp
+
+if [ -d $PUPFILES_DIR/.git ]; then
+	echo "pulling"
+	cd $PUPFILES_DIR
+	git pull
+	cd -
+else
+	git clone https://github.com/MikeCobra/.pupfiles.git $PUPFILES_DIR
+fi
+/opt/puppetlabs/bin/puppet apply --modulepath $PUPFILES_DIR/modules $PUPFILES_DIR/manifests/desktop.pp
