0002077d0084023d3abc94b0c20e6bc1a902e1ec
Mike Clarke <michaelclarkecs@gmail.com>
2015-11-01 19:14:38 +0000

    Fix file paths

--- modules/konsole/manifests/init.pp
+++ modules/konsole/manifests/init.pp
@@ -1,7 +1,7 @@
 class konsole {
   file { '/home/michaelc/.local/share/konsole/base16-yesterdaybright.colorscheme':
     ensure => file,
-    source => 'puppet://modules/konsole/base16-yesterdaybright.colorscheme',
+    source => 'puppet:///modules/konsole/base16-yesterdaybright.colorscheme',
     owner  => 'michaelc',
     group  => 'michaelc',
     mode   => '0644',
@@ -9,7 +9,7 @@ class konsole {
 
   file { '/home/michaelc/.local/share/konsole/Shell.profile':
     ensure => file,
-    source => 'puppet://modules/konsole/Shell.profile',
+    source => 'puppet:///modules/konsole/Shell.profile',
     owner  => 'michaelc',
     group  => 'michaelc',
     mode   => '0644',
